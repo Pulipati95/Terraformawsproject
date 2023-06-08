4b4f98c4845fb8f147a3ea6ceb7cbe66b0b9188f
Pulipati95 <135943553+Pulipati95@users.noreply.github.com>
2023-06-08 12:03:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,6 +87,7 @@ resource "aws_instance" "web" {
   ami                         = "ami-024e6efaf93d85776"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.main.id
+  key_name = "terraform"
   associate_public_ip_address = true
   vpc_security_group_ids      = ["${aws_security_group.allow_web.id}"]
 
