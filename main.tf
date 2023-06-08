d597593409812832b7f5bdd567d1d204376a8609
Pulipati95 <135943553+Pulipati95@users.noreply.github.com>
2023-06-08 11:25:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 terraform {
   
-  required_providers {
-      aws = {
-          source = "hashicorp/aws"
-          version = "4.5.0"
-      }
+  backend "s3" {
+    bucket = "aws-bucket-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
   }
 }
 
